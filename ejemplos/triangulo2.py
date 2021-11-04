66245eb341927dc9fe35dcc5df303da789a96c4d
Pythonistas2021 <93458080+Pythonistas2021@users.noreply.github.com>
2021-11-04 09:51:42 -0500

    Update triangulo2.py

--- ejemplos/triangulo2.py
+++ ejemplos/triangulo2.py
@@ -3,7 +3,7 @@
 # Para el tamaño del triángulo
 n = int(input("Ingrese el valor máximo del triángulo: "))
 
-# Para el primer número
+# Para el primer triángulo
 for i in range(1,n+1):
     for j in range(i):
         print(j+1, end=" ")
