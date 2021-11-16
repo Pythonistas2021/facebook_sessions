651fdc4600468e5fb68865b09600d6ddf64a7122
rsgbMint <pythonistasdaro@gmail.com>
2021-11-16 01:13:52 -0500

    combinatoria exercise of HackerRank

--- ejemplos/combinatoria.py
+++ ejemplos/combinatoria.py
@@ -3,21 +3,20 @@ def comb(N, k):
     factorial_k = 1
     factorial_Nk = 1
 
-    for i in range(1, N):
+    for i in range(1, N+1):
         factorial_N = factorial_N*i
     
-    for j in range(1, k):
+    for j in range(1, k+1):
         factorial_k = factorial_k*j
 
-    for nk in range(1, N-k):
+    for nk in range(1, N-k+1):
         factorial_Nk = factorial_Nk*nk
 
-    combinatoria = factorial_N/(factorial_k*factorial_Nk)
+    combinatoria = int(factorial_N/(factorial_k*factorial_Nk))
 
-    print(f"La combinatoria de N={N} y k={k} es:", combinatoria)
+    print(combinatoria)
 
 N = int(input("N = "))
 k = int(input("k = "))
 
 comb(N, k)
-
